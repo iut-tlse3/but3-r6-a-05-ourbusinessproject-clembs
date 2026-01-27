6c918d337ed238628e24bf94bab7fecb67d5046c
Clembs <clembs@clembs.com>
2026-01-27 16:42:21 +0100

    fix #1.1 classe Project
    Signed-off-by: Clembs <clembs@clembs.com>

---
+++ src/main/java/ourbusinessproject/Project.java
@@ -0,0 +1,25 @@
+package ourbusinessproject;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public class Project {
+    @NotEmpty
+    private String title;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
